2c9ead5f7d1211ab6a18f3b50f6f37c2c47d57c8
KJ <kj.schmidt913@gmail.com>
2021-05-27 12:33:56 -0500

    alginment for nav works for desktop and mobile. added links. need a link for docs

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -7,16 +7,15 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand mx-3" href="#">
+        <a class="navbar-brand mx-sm-3 " href="#">
             <img alt="Vue logo" src="../assets/foundry.png" class="w-50">
         </a>
-        
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="col-2 d-none d-md-block"></div>
+        <div class="collapse navbar-collapse col-4" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-            <router-link to="/"><a class="nav-item nav-link active" href="#">Home</a></router-link>
-            <a class="nav-item nav-link" href="#">Features</a>
-            <a class="nav-item nav-link" href="#">Pricing</a>
-            <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                <router-link to="/about"><a class="nav-item nav-link" href="#">About</a></router-link>
+                <a class="nav-item nav-link" target="blank" href="https://github.com/MLMI2-CSSI/foundry">Documentation</a>
+                <a class="nav-item nav-link " target="blank" href="https://github.com/MLMI2-CSSI/foundry/tree/main/examples">Examples</a>
             </div>
         </div>
     </nav>
