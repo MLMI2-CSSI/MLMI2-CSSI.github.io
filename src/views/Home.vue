2b8399637d4c042b88a9ea4e952c86dbb31a131d
KJ <kj.schmidt913@gmail.com>
2021-06-09 14:41:07 -0500

    added some content. realized that with new initiative (publishing not searching) our layout didnt really work so its a mess rn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,70 @@
 <template>
-  <div class="home">
-    home
+  <div class="home col-12 mb-5">
+    <div class="container py-5 mt-1 col-12 bg-light">
+      <!-- heading content -->
+      <div class="jumbotron text-center">
+        <h1 class="display-4">
+          An open source machine learning platform for scientists
+        </h1>
+        <p class="lead mt-3">
+          Access datasets and ML models while setting industry benchmarks
+        </p>
+        <a href="#"
+          ><button class="btn btn-primary btn-lg mt-3" role="button">
+            Learn more
+          </button></a
+        >
+      </div>
+    </div>
+
+    <!-- middle content -->
+    <div class="row my-5">
+      <div class="col-10 mx-auto">
+        <div class="row">
+          <div class="col-sm-4 mx-auto">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Find Datasets</h5>
+                <p class="card-text">We have datasets.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4 mx-auto">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Find Models</h5>
+                <p class="card-text">We have models.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- be apart of foundry section -->
+
+    <div class="mx-auto col-7 mt-5">
+      <div class="row mx-auto">
+        <div class="col-3 bg-secondary">
+          :)
+          <p>I will be an image</p>
+        </div>
+        <div class="col-9">
+          <h1>Build Foundry With Us</h1>
+          <p>Publish your data on Foundry so others can access it as well.</p>
+          <a href="#"
+            ><button class="btn btn-primary btn-sm mt-3" role="button">
+              Publish data
+            </button></a
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Home'
-}
+  name: "Home",
+};
 </script>
