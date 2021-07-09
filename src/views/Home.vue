b1c436fd21ae79ec7899634e75790c22539ace4f
KJ <kj.schmidt913@gmail.com>
2021-07-09 14:21:21 -0500

    added updated copy. still need to add another section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,17 +21,23 @@
         <div class="row">
           <div class="col-sm-4 mx-auto">
             <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Datasets</h5>
-                <p class="card-text">We host datasets </p>
+              <div class="card-body text-center">
+                <h5 class="card-title">Add your dataset</h5>
+                <p class="card-text">
+                  We host data from a variety of scientific domains and make it
+                  easy to share and use.
+                </p>
               </div>
             </div>
           </div>
           <div class="col-sm-4 mx-auto">
             <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">ML Models</h5>
-                <p class="card-text">We host models.</p>
+              <div class="card-body text-center">
+                <h5 class="card-title">Add your model</h5>
+                <p class="card-text">
+                  Our models run in the cloud, which makes them easy to
+                  implement and share.
+                </p>
               </div>
             </div>
           </div>
@@ -42,20 +48,30 @@
     <!-- be apart of foundry section -->
 
     <div class="mx-auto col-7 mt-5">
-      <div class="row mx-auto">
-        <div class="col-3 bg-secondary">
-          :)
-          <p>I will be an image</p>
-        </div>
-        <div class="col-9">
-          <h1>Build Foundry With Us</h1>
-          <p>Publish your data on Foundry so others can access it as well.</p>
-          <a href="#"
-            ><button class="btn btn-primary btn-sm mt-3" role="button">
-              Publish data
-            </button></a
+      <div class="col-9 mx-auto">
+        <h1 class="text-center">Build Foundry With Us</h1>
+        <p>
+          At Foundry, we believe accessibility and collaboration are the keys to
+          problem solving. That's why we've made it super easy for you to share
+          and use data. If you have a dataset you'd like to contribute, our
+          <a href="https://github.com/MLMI2-CSSI/foundry/tree/main/examples/publishing-guides" target="blank">publishing guide</a> can set you up to do
+          that.
+        </p>
+        <p>
+          Check out our
+          <a
+            href="https://ai-materials-and-chemistry.gitbook.io/foundry/v/docs/"
+            target="blank"
+            >documentation</a
           >
-        </div>
+          to see how easy it is to use Foundry, and follow our
+          <a
+            href="https://github.com/MLMI2-CSSI/foundry/tree/main/examples"
+            target="blank"
+            >examples</a
+          >
+          to do it all yourself.
+        </p>
       </div>
     </div>
   </div>
