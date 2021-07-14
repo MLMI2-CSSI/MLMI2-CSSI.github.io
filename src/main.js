60a1b0eabc14d29de89884d0ee4d5d0bdd896c2f
KJ <kj.schmidt913@gmail.com>
2021-07-14 11:24:59 -0500

    installed code highlighter but styling not coming through. working on new section layout. addeda few icons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueHighlightJS from 'vue-highlightjs'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -11,6 +12,8 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+// for code snippets
+Vue.use(VueHighlightJS)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,6 +22,10 @@
           <div class="col-sm-4 mx-auto">
             <div class="card">
               <div class="card-body text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-file-bar-graph" viewBox="0 0 16 16">
+  <path d="M4.5 12a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-1zm3 0a.5.5 0 0 1-.5-.5v-4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1zm3 0a.5.5 0 0 1-.5-.5v-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-.5.5h-1z"/>
+  <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
+</svg>
                 <h5 class="card-title">Add your dataset</h5>
                 <p class="card-text">
                   We host data from a variety of scientific domains and make it
@@ -33,9 +37,13 @@
           <div class="col-sm-4 mx-auto">
             <div class="card">
               <div class="card-body text-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+  <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+  <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
+</svg>
                 <h5 class="card-title">Add your model</h5>
                 <p class="card-text">
-                  Our models run in the cloud, which makes them easy to
+                  Our models live in the cloud, which makes them easy to
                   implement and share.
                 </p>
               </div>
@@ -43,6 +51,29 @@
           </div>
         </div>
       </div>
+    </div>
+    <!-- Starter code and metadata section -->
+    <div class="col-8 my-5 mx-auto">
+      <h2>whatever this is</h2>
+      <div class="row">
+        <div class="col-7">
+          <pre
+            v-highlightjs
+            class="bg-light"
+          ><code class="python">const s = new Date().toString(){
+  hi }</code></pre>
+        </div>
+        <div class="col-4">text here</div>
+      </div>
+      <!-- end of row -->
+      <div class="col-12 text-right ">
+        <h2 class="col-12">whatever this is</h2>
+      </div>
+      <div class="row">
+        <div class="col-4">text here</div>
+        <div class="col-7">metadata here</div>
+      </div>
+      <!-- end of row -->
     </div>
 
     <!-- be apart of foundry section -->
@@ -54,8 +85,12 @@
           At Foundry, we believe accessibility and collaboration are the keys to
           problem solving. That's why we've made it super easy for you to share
           and use data. If you have a dataset you'd like to contribute, our
-          <a href="https://github.com/MLMI2-CSSI/foundry/tree/main/examples/publishing-guides" target="blank">publishing guide</a> can set you up to do
-          that.
+          <a
+            href="https://github.com/MLMI2-CSSI/foundry/tree/main/examples/publishing-guides"
+            target="blank"
+            >publishing guide</a
+          >
+          can set you up to do that.
         </p>
         <p>
           Check out our
@@ -76,6 +111,9 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+</style>
 
 <script>
 export default {
