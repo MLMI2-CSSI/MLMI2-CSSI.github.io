a22e260f104f2ee4cc34fa114020622bb7101009
KJ <kj.schmidt913@gmail.com>
2021-07-14 12:49:47 -0500

    added basic idea for code sippet with data image and link to notebook with that data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -56,24 +56,27 @@
       <h1 class="mb-4">We make collaboration easy</h1>
       <div class="row mb-5">
         <div class="col-6">
-          <pre v-highlightjs class="bg-light p-2"><code class="python">
-f = f.load("_test_foundry_stan_dendrite_segmentation_v1.1", globus=False)
-            
+          <pre v-highlightjs class="bg-light px-4"><code class="python">
+f = f.load("your_dataset")           
 X,y = f.load_data()
-
-  </code></pre>
+  </code>
+  <img
+          alt="dendrite segmentation data snippet"
+          src="../assets/dendrite-seg-img.png"
+          class="w-100 pb-3"
+        /></pre>
         </div>
         <div class="col-5">
           <p class="fw-light fs-4">
             Once a dataset is uploaded to Foundry, you can load it with just 2
             lines of code.
           </p>
-          <button class="btn btn-primary">See Examples</button>
+          <a href="https://colab.research.google.com/github/MLMI2-CSSI/foundry/blob/main/examples/dendrite-segmentation/dendrite_segmentation.ipynb" target="blank"><button class="btn btn-primary">Run this in Google Colab</button></a>
         </div>
       </div>
       <!-- end of row -->
       <div class="col-12 text-end">
-        <h1 class="col-12 my-5">and save you valuable time</h1>
+        <h1 class="col-11 my-5">and save you valuable time</h1>
       </div>
       <div class="row">
         <div class="col-6">
@@ -94,8 +97,8 @@ X,y = f.load_data()
     <hr class="col-10 m-auto"/>
 
     <div class="mx-auto col-7 mt-5">
-      <div class="col-9 mx-auto">
-        <h1 class="text-center">Build Foundry With Us</h1>
+      <div class="col-11 mx-auto">
+        <h1 class="text-center my-3 display-4">Build Foundry With Us</h1>
         <p>
           At Foundry, we believe accessibility and collaboration are the keys to
           problem solving. That's why we've made it super easy for you to share
